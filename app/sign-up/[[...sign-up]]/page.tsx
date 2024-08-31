86b57eb65602ea8feb6a6c1df00c099110897a63
Niclas <niclastanskanen@hotmail.com>
2024-08-31 10:29:05 +0200

    add signup page

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+
+// import VerificationForm from './VerificationForm'
+import SignUpForm from './SignUpForm'
+
 
 const page = () => {
+  const [verifying, setVerifying] = useState(false)
+  const options = {
+    appearance: {
+      theme: 'stripe'
+    }
+  }
+
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string)
+
+  if (verifying) {
+    return <VerificationForm />
+  }
+
   return (
-    <div>page</div>
+    <div className='mt-20 flex items-center justify-center'>
+      {/* @ts-ignore */}
+      <Elements options={options} stripe={stripePromise}>
+        <SignUpForm setVerifying={setVerifying} />
+      </Elements>
+    </div>
   )
 }
 
