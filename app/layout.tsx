024a48ecd240a2194dba3a9975499ff29feebeee
Niclas <niclastanskanen@hotmail.com>
2024-08-31 10:29:24 +0200

    add shadcn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <SignedOut>
-            <SignInButton />
+            {/* <SignInButton /> */}
           </SignedOut>
           <SignedIn>
             <UserButton />
